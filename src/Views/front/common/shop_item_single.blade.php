6d9415b6e9521bbc75172f5307ab68021d5803c3
GP247 <gp247.net@gmail.com>
2025-04-27 09:32:41 +0700

    Update template

--- src/Views/front/common/shop_item_single.blade.php
+++ src/Views/front/common/shop_item_single.blade.php
@@ -2,9 +2,9 @@
     <div class="product-body">
       <div class="product-figure">
           <a href="{{ $item->getUrl() }}">
-          <img src="{{ gp247_file($item->getThumb()) }}" alt="{{ $item->name }}"/>
+          <img src="{{ gp247_file($item->getThumb()) }}" alt="{{ $item->name ?? $item->title ?? '' }}"/>
           </a>
       </div>
-      <h5 class="product-title"><a href="{{ $item->getUrl() }}">{{ $item->name }}</a></h5>
+      <h5 class="product-title"><a href="{{ $item->getUrl() }}">{{ $item->name ?? $item->title ?? '' }}</a></h5>
     </div>
 </article>
